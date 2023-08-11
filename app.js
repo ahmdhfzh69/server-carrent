a2e9359233b57801d6f197cd55dee350e55591cd
ahmdhfzh69 <ahmdhfzh69@users.noreply.github.com>
2023-08-11 17:27:22 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -50,6 +50,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
