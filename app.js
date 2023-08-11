4af78261c03b97a6dd035d9d6c92da863cb61e3d
ahmdhfzh69 <ahmdhfzh69@users.noreply.github.com>
2023-08-11 18:00:54 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -59,10 +59,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-});
+})
 module.exports = app;
